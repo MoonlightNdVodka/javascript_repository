2062600b884df52a47845766cf11994230db42e7
Egor <411441azaza@gmail.com>
2022-01-12 11:09:42 +0300

    завершил курс основы javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1226,3 +1226,101 @@ export default function sayHiDef() {
 
 // можно в индексе подключать модулями файлы js, но нужно настраивать экспорты и полностью прописывать пути.
 // <script type='module' src="./dist/js/bundle.js"></script>
+
+
+// ОБРАБОТКА ОШИБОК
+
+try {
+    console.log('Normal');
+    console.log(a); //намеренная ошибка
+    console.log('result');
+} catch (error) {
+    //error  отработает вслучае ошибки выше и ее можно будет разобрать
+    //на составляющие
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+} finally {
+    //выполнится при любом исходе
+}
+
+//после try код продолжит выполнение, когда в обычном случае
+//ошибка вызовет остановку кода
+console.log('Still work');
+
+//Babel - трансформирует новый код для старых браузеров  
+//Polyfil - определенные участки кода, которые эмулируют поведение современного кода  
+//coreJS - стандартная библ для ДЖСа,
+//  которая содержит максимально полный набор полифилов
+// corejs: 3,
+//функция ниже помогает выбирать полифилы только те, которые исп-ся в коде
+//useBuiltIns: "usage"
+
+
+// ANGULAR что бы знать:
+// -node.js//install
+//-TypeScript - understand & knowlege
+//-Webpack - knowlege
+//-MVC-pattern
+//-Angular
+
+// REACT что бы знать:
+// -node.js//install
+// JSX preprocessor
+// Babel
+// Webpack
+// React
+
+// Vue.js что бы знать:
+// -node.js//install
+// Webpack
+// Vue
+
+
+
+//работа с jQuery
+// работа с библиотекой при разработке проекта с нуля не обязательна, большинство функций уже реализовано в нативном JS
+// при поддержке старых проектов знание библиотеки может понадобиться
+// основная навигация и документация по библиотеке здесь:
+// https://page2page.lohmach.info/index.php5/Заглавная_страница.html
+
+
+// Функции генераторы function*
+// Генераторы являются функциями с возможностью выхода и последующего входа. 
+// Их контекст исполнения (значения переменных) сохраняется при последующих входах.
+function* generator() {
+	yield 'S';
+	yield 'C';
+	yield 'P';
+}
+
+const str = generator();
+console.log(str.next().value);
+console.log(str.next().value);
+console.log(str.next().value);
+
+function* count(n) {
+	for (let i = 0; i < n; i++) {
+		yield i;
+	}
+}
+//генератор можно запустить передав нужный итератор
+const counter = count(4);
+console.log(counter.next().value);
+console.log(counter.next().value);
+console.log(counter.next().value);
+console.log(counter.next().value);
+console.log(counter.next().value);
+
+
+//функцию генератор можно перебрать
+//  через цикл что бы не вызывать постояно консоль лог
+for (let k of count(7)) {
+	console.log(k);
+}
+
+// ГОТОВЫЕ РЕШЕНИЯ ДЛЯ КОДА
+
+// желательно использовать плагины, которые не использую какх то зависимостей, типо jquery и тд.
+//популярные слайдеры: OwlCarousel, Slick(jquery), tiny-slider, galleria, fotorama, glide 
+// https://nisnom.com/category/veb-razrabotki/
